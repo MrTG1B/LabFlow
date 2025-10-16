c1450032fa572f2f2f098b19dccfa9b10fdd8440
Lumen Labs <lumen.labstec@gmail.com>
2025-10-16 11:10:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9285069868-7440d",
+  "appId": "1:711332214691:web:b578651f25a39f0e048c16",
+  "apiKey": "AIzaSyDzrIWhbYyAVPVBXWRrPODxg2H_BfXGbnU",
+  "authDomain": "studio-9285069868-7440d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "711332214691"
+};
